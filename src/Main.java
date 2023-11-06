c1aee3366e1c5410c3141455bbaf5d937b77da58
JOAOvitorUU <jv141595@gmail.com>
2023-11-06 09:14:28 -0300

    Polimorfismo

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Pessoa qualquer = new Pessoa();
+        Esportista jogadora = new Esportista();
+        jogadora.setNome("Taissa");
+        // Polimorfismo
+        qualquer = new Esportista();
+        //jogadora = (Esportista) new Pessoa();
     }
+
 }
---
+++ src/Pessoa.java
@@ -0,0 +1,47 @@
+import java.time.LocalDate;
+
+public class Pessoa {
+    //Atributos
+    private String nome;
+    private LocalDate dataNascimento;
+    private double altura;
+    private double peso;
+    // Construtores, getters e setters
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public Pessoa(){
+
+    }
+
+}
